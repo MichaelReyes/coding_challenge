648f1167e6006e0654a993e2ef5ec0d79a823e07
Michael Angelo Reyes <michaelangelo.reyes19@gmail.com>
2022-05-20 12:21:56 +0800

    :construction: DashboardActivity.kt as parent activity and PostsFragment.kt init :zap: remove themes.xml and replace with basic style

---
+++ app/src/main/java/com/app/tech/codingchallenge/feature/dashboard/DashboardActivity.kt
@@ -0,0 +1,22 @@
+package com.app.tech.codingchallenge.feature.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import com.app.tech.codingchallenge.R
+import com.app.tech.codingchallenge.core.base.BaseActivity
+import com.app.tech.codingchallenge.core.base.BaseViewModel
+import com.app.tech.codingchallenge.databinding.ActivityDashboardBinding
+
+class DashboardActivity : BaseActivity<ActivityDashboardBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.activity_dashboard
+
+    override fun getNavController(): NavController? = null
+
+    override fun getViewModel(): BaseViewModel? = null
+
+    override fun onCreated(instance: Bundle?) {
+        setSupportActionBar(binding.toolbar)
+    }
+}
